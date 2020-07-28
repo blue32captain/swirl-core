ce5c0fbed7b7bd170f11cf52ec7785036857e3a2
bluecaptain <blue32captain@gmail.com>
2020-07-29 00:29:21 +0300

    docs update

--- migrations/2_deploy_hasher.js
+++ migrations/2_deploy_hasher.js
@@ -1,6 +1,21 @@
 /* global artifacts */
-const Hasher = artifacts.require('Hasher')
+const path = require('path')
 
-module.exports = async function(deployer) {
-  await deployer.deploy(Hasher)
+const genContract = require('circomlib/src/mimcsponge_gencontract.js')
+const Artifactor = require('@truffle/artifactor')
+
+module.exports = function(deployer) {
+  return deployer.then( async () =>  {
+    const contractsDir = path.join(__dirname, '..', 'build/contracts')
+    let artifactor = new Artifactor(contractsDir)
+    let contractName = 'Hasher'
+    await artifactor.save({
+      contractName,
+      abi: genContract.abi,
+      unlinked_binary: genContract.createCode('mimcsponge', 220),
+    }).then(async () => {
+      const hasherContract = artifacts.require(contractName)
+      await deployer.deploy(hasherContract)
+    })
+  })
 }
