1d764bac574ea4c162f610080d55baf54963d770
bluecaptain <blue32captain@gmail.com>
2021-05-06 03:19:51 +0300

    merkle tree update - getIndexByElement

--- relay/relay.js
+++ relay/relay.js
@@ -1,24 +1,19 @@
-// This is still WiP
-const bigInt = require('snarkjs/src/bigint')
+let bigInt = require('snarkjs/src/bigint')
 const utils = require('../scripts/utils')
 
 const express = require('express')
 const app = express()
 app.use(express.json())
 
-// todo get from config
-const RPC_ENDPOINT = 'http://localhost:8545'
-const NET_ID = 42
-// const PRIVATE_KEY = ''
-
 const Web3 = require('web3')
-const web3 = new Web3(RPC_ENDPOINT, null, { transactionConfirmationBlocks: 1 })
+const web3 = new Web3('http://localhost:8545', null, { transactionConfirmationBlocks: 1 })
 const contractJson = require('../build/contracts/Mixer.json')
-const mixer = new web3.eth.Contract(contractJson.abi, contractJson.networks[NET_ID].address)
+let netId = 42
+const mixer = new web3.eth.Contract(contractJson.abi, contractJson.networks[netId].address)
 
 function getMinimumFee() {
   // todo calc acceptable fee
-  return bigInt(1e16)
+  return 1e16
 }
 
 app.post('/deposit', async (req, resp) => {
@@ -36,17 +31,9 @@ app.post('/deposit', async (req, resp) => {
   }
 
   try {
-    const gas = await mixer.withdraw(proof.pi_a, proof.pi_b, proof.pi_b, proof.publicSignals).estimateGas()
-    if (gas > 1e6) {
-      // something is wrong
-    }
-    const result = mixer.withdraw(proof.pi_a, proof.pi_b, proof.pi_b, proof.publicSignals).send()
-    result.once('transactionHash', function(hash){
-      resp.send({ transaction: hash })
-    }).on('error', function(e){
-      console.log(e)
-      resp.status(400).send('Transaction was reverted')
-    })
+    let receipt = await mixer.withdraw(proof.pi_a, proof.pi_b, proof.pi_b, proof.publicSignals)
+    console.log(receipt)
+    resp.send({ transaction: receipt.transactionHash })
   } catch (e) {
     console.log(e)
     resp.status(400).send('Transaction was reverted')
